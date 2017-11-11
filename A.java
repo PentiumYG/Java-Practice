8943ccc526168aebb98ce63fef2363e522c9c311
ukigos <yuktig.jaipur@gmail.com>
2017-11-11 19:23:05 +0530

    Java File

---
+++ A.java
@@ -0,0 +1,13 @@
+class A
+{
+   int x=90;
+  public static void main(String[] args)
+{
+  int y=180;
+  A obj= new A();
+ System.out.println("Global=" + obj.x);
+ System.out.println("local=" + y);
+
+}
+
+}
